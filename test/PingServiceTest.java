69bf1c22f6518a24a89bc8a8e5722fd4da0581c7
gati3478 <giopetriashvili@gmail.com>
2015-06-03 22:34:57 +0400

    Removed redundant files

---
+++ test/PingServiceTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+import ge.edu.freeuni.sdp.xo.signin.PingService;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import org.junit.Test;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+
+public class PingServiceTest extends JerseyTest {
+
+	@Override
+	protected Application configure() {
+		return new ResourceConfig(PingService.class);
+	}
+
+	@Test
+	public void testGet() {
+		Response actual = target("ping").request().get();
+		assertEquals(Response.Status.OK.getStatusCode(), actual.getStatus());
+	}
+
+}
