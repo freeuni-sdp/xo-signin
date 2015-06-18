47e3942e05de1dd43887621e383d59e67e40a030
gati3478 <giopetriashvili@gmail.com>
2015-06-18 23:12:48 +0400

    Added fake repository implementation

--- src/main/java/ge/edu/freeuni/sdp/xo/signin/data/InMemoryRepository.java
+++ src/main/java/ge/edu/freeuni/sdp/xo/signin/data/InMemoryRepository.java
@@ -1,61 +1,82 @@
 package ge.edu.freeuni.sdp.xo.signin.data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import ge.edu.freeuni.sdp.xo.signin.data.entity.SignInInfoEntity;
 import ge.edu.freeuni.sdp.xo.signin.data.entity.UserInfoEntity;
+import ge.edu.freeuni.sdp.xo.signin.data.json.UserInfo;
 
 import com.microsoft.azure.storage.StorageException;
 
 public class InMemoryRepository implements Repository {
+	Map<String, SignInInfoEntity> mUsers = new HashMap<String, SignInInfoEntity>();
+	Map<String, SignInInfoEntity> mEmails = new HashMap<String, SignInInfoEntity>();
+	Map<String, UserInfoEntity> mTokens = new HashMap<String, UserInfoEntity>();
 
 	private static Repository instance;
 
+	private InMemoryRepository(Map<String, SignInInfoEntity> usernames,
+			Map<String, SignInInfoEntity> emails,
+			Map<String, UserInfoEntity> tokens) {
+		mUsers = usernames;
+		mEmails = emails;
+		mTokens = tokens;
+	}
+
 	public static Repository getInstance() {
 		if (instance == null) {
-			instance = new InMemoryRepository();
+			Map<String, SignInInfoEntity> usernames = new HashMap<String, SignInInfoEntity>();
+			Map<String, SignInInfoEntity> emails = new HashMap<String, SignInInfoEntity>();
+			Map<String, UserInfoEntity> tokens = new HashMap<String, UserInfoEntity>();
+			instance = new InMemoryRepository(usernames, emails, tokens);
 		}
 		return instance;
 	}
 
 	@Override
 	public void insertOrUpdate(SignInInfoEntity entity) throws StorageException {
-		// TODO Auto-generated method stub
-
+		mUsers.put(entity.getUsername(), entity);
+		mEmails.put(entity.getEmail(), entity);
 	}
 
 	@Override
 	public UserInfoEntity findByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		SignInInfoEntity entity = mEmails.get(email);
+		UserInfo info = new UserInfo();
+		info.setEmail(entity.getEmail());
+		info.setUsername(entity.getUsername());
+		return UserInfoEntity.fromUserInfo(info);
 	}
 
 	@Override
 	public UserInfoEntity findByUsername(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		SignInInfoEntity entity = mUsers.get(username);
+		UserInfo info = new UserInfo();
+		info.setEmail(entity.getEmail());
+		info.setUsername(entity.getUsername());
+		return UserInfoEntity.fromUserInfo(info);
 	}
 
 	@Override
 	public UserInfoEntity findForToken(String token) {
-		// TODO Auto-generated method stub
-		return null;
+		return mTokens.get(token);
 	}
 
 	@Override
 	public boolean hasToken(String token) {
-		// TODO Auto-generated method stub
-		return false;
+		return mTokens.containsKey(token);
 	}
 
 	@Override
 	public void deleteToken(String token) {
-		// TODO Auto-generated method stub
-
+		mTokens.remove(token);
 	}
 
 	@Override
 	public void insertToken(String token, String email) {
-		// TODO Auto-generated method stub
-
+		UserInfoEntity entity = findByEmail(email);
+		mTokens.put(token, entity);
 	}
 
 	@Override
