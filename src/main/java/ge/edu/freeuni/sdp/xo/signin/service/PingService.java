fd675f1fa180c615879970d55d1d34a3c183a8fc
gati3478 <giopetriashvili@gmail.com>
2015-06-19 13:08:18 +0400

    Fixed ping address

--- src/main/java/ge/edu/freeuni/sdp/xo/signin/service/PingService.java
+++ src/main/java/ge/edu/freeuni/sdp/xo/signin/service/PingService.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("ping")
+@Path("webapi/ping")
 @Consumes({ MediaType.APPLICATION_JSON })
 @Produces({ MediaType.APPLICATION_JSON })
 public class PingService {
