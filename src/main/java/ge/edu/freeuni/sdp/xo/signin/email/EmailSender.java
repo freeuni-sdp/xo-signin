a561c9b960d8b0f00341924872d84db13ae27681
gati3478 <giopetriashvili@gmail.com>
2015-07-02 17:35:44 +0400

    Added email distribution features for accounts

---
+++ src/main/java/ge/edu/freeuni/sdp/xo/signin/email/EmailSender.java
@@ -0,0 +1,64 @@
+package ge.edu.freeuni.sdp.xo.signin.email;
+
+import com.sendgrid.SendGrid;
+import com.sendgrid.SendGridException;
+
+import ge.edu.freeuni.sdp.xo.signin.data.json.SigninInfo;
+import ge.edu.freeuni.sdp.xo.signin.data.json.Token;
+
+public class EmailSender {
+	private static final String FROM = "noreply@xo-signin.herokuapp.com";
+
+	public EmailSender() {
+	}
+
+	public static void sendActivationEmail(SigninInfo signinInfo, Token token) {
+		SendGrid sendgrid = new SendGrid(SendGridInfo.USERNAME, SendGridInfo.PASSWORD);
+
+		SendGrid.Email email = new SendGrid.Email();
+		email.addTo("giopetriashvili@gmail.com");
+		email.setFrom(FROM);
+		email.setSubject("Account Confirmation");
+		email.setText("Follow this link to connfirm account: " + "http://xo-webui.herokuapp.com/confirm_account?token="
+				+ token.getToken() + "&username=" + signinInfo.getUsername() + "&email=" + signinInfo.getEmail());
+
+		try {
+			sendgrid.send(email);
+		} catch (SendGridException e) {
+			System.err.println(e);
+		}
+	}
+
+	public static void sendUserInfoEmail(SigninInfo signinInfo) {
+		SendGrid sendgrid = new SendGrid(SendGridInfo.USERNAME, SendGridInfo.PASSWORD);
+
+		SendGrid.Email email = new SendGrid.Email();
+		email.addTo("giopetriashvili@gmail.com");
+		email.setFrom(FROM);
+		email.setSubject("Account Information");
+		email.setText("Username: " + signinInfo.getUsername() + "\nPassword: " + signinInfo.getPassword());
+
+		try {
+			sendgrid.send(email);
+		} catch (SendGridException e) {
+			System.err.println(e);
+		}
+	}
+
+	public static void sendUsernameEmail(SigninInfo signinInfo) {
+		SendGrid sendgrid = new SendGrid(SendGridInfo.USERNAME, SendGridInfo.PASSWORD);
+
+		SendGrid.Email email = new SendGrid.Email();
+		email.addTo("giopetriashvili@gmail.com");
+		email.setFrom(FROM);
+		email.setSubject("Email Recovery");
+		email.setText("Username: " + signinInfo.getUsername());
+
+		try {
+			sendgrid.send(email);
+		} catch (SendGridException e) {
+			System.err.println(e);
+		}
+	}
+
+}
---
+++ src/main/java/ge/edu/freeuni/sdp/xo/signin/email/SendGridInfo.java
@@ -0,0 +1,6 @@
+package ge.edu.freeuni.sdp.xo.signin.email;
+
+public final class SendGridInfo {
+	public static final String USERNAME = "gati3478";
+	public static final String PASSWORD = "3478gati";
+}
